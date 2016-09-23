5987959c033e5402d4cbe5cabffde0982d2e1d71
Federico <admin@imgdigital.com.ar>
2016-09-23 11:27:29 -0300

    First Commit

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * storefront child theme functions.php file.
+ *
+ * @package storefront-child
+ */
+
+// DO NOT REMOVE THIS FUNCTION AS IT LOADS THE PARENT THEME STYLESHEET
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
+function enqueue_parent_theme_style() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+/* Add any custom PHP below this line of text */
+
+/**
+ * Load IMGD Framework compatibility file.
+ */
+require get_template_directory() . '/imgd/imgd_funciones.php';
